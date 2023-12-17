24b9bf7f17b74cf9c36ee9fd43af596efb5dfc86
Younes Outerbah <younesouterbah1@gmail.com>
2023-12-17 22:03:50 +0100

    done with workout controller

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,6 @@
 const { Workout } = require("../models/workoutModel");
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
 
 const getWorkouts = async (req, res) => {
   const workout = await Workout.find({});
@@ -21,15 +23,47 @@ const addWorkout = async (req, res) => {
 };
 
 const getWorkout = async (req, res) => {
-  res.json({ msg: "get one workout" });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(404).json({ err: "no workout" });
+    }
+    const workout = await Workout.findById(req.params.id);
+
+    //   if workout dosent exists
+    if (!workout) {
+      res.status(400);
+      return res.json({ msg: "Workout Not Found" });
+    }
+
+    res.status(200).json(workout);
+  } catch (error) {
+    return res.status(404).json(error);
+  }
 };
 
 const editWorkout = async (req, res) => {
-  res.json({ msg: "edit one workout" });
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ err: "no workout found by that id" });
+  }
+  const workout = await Workout.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!workout) {
+    res.status(404);
+    return res.json({ error: "error happend" });
+  }
+  res.json(workout);
 };
 
 const deleteWorkout = async (req, res) => {
-  res.json({ msg: "workout deleted" });
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ err: "no workout" });
+  }
+  const workout = await Workout.findByIdAndDelete(req.params.id);
+
+  if (!workout) {
+    return res.status(400).json({ error: "Workout Not found" });
+  }
+
+  res.status(200).json({ msg: "Workout Has Been Deleted" });
 };
 
 module.exports = {
