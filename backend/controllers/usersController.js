442a6f81769fa1daeeb892038e0890471138de27
Younes Outerbah <younesouterbah1@gmail.com>
2023-12-22 21:31:32 +0100

    added bcrypt to hash password

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,15 +1,36 @@
 const { User } = require("../models/userModel");
-
+const bcrype = require("bcrypt");
 
 // login
 const userLogin = async (req, res) => {
   res.json({ msg: "user logged in" });
-  
 };
 
 // sign up
 const userSign = async (req, res) => {
-  res.json({ msg: "user siggend up" });
+  const { email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    return res.status(400).json({ error: "Email already in use" });
+  }
+
+  const salt = await bcrype.genSalt(10);
+  const hashedPassword = await bcrype.hash(password, salt);
+
+  const user = await new User({
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    user.save();
+    res.status(201).json({ email, password, user });
+  } else {
+    res.status(400);
+    throw new Error("invalid data");
+  }
 };
 
 module.exports = {
